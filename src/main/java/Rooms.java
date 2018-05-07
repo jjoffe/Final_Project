f14e7641f3bb491f3861a4ee2cb73af35d9c4ae0
Anna ¯\_(ツ)_/¯ <annacaine@172-1-0-100.lightspeed.hstntx.sbcglobal.net>
2018-05-07 10:19:48 -0400

    added the JSON file

---
+++ src/main/java/Rooms.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class Rooms {
+
+    Double id;
+    ArrayList<Double> exits;
+    ArrayList<String> objects;
+    String text;
+
+    public Rooms(){
+
+    }
+}
